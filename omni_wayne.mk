644a4109c741bd4e76eb32741c3374893dd9e20a
adiatul16 <adiatul16@gmail.com>
2021-03-11 22:21:40 +0530

    wayne: Adapt for OFRP

--- omni_wayne.mk
+++ omni_wayne.mk
@@ -23,7 +23,7 @@ $(call inherit-product, build/target/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit language packages
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
